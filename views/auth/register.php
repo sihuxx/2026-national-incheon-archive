d822cf0dc33ce528c45e480b38d31c399153ced6
sihuxx <sihu714@gmail.com>
2026-06-22 16:09:24 +0900

    폼 디자인 통일

--- views/auth/register.php
+++ views/auth/register.php
@@ -4,8 +4,10 @@ if($user) back("비회원 유저만 접근 가능한 페이지입니다");
 ?>
 
 <main class="auth-page">
-    <div class="intro__main">회원가입</div>
     <form action="/signup" method="post" enctype="multipart/form-data" class="default-form">
+        <div class="form-header">
+            <h3>회원가입</h3>
+        </div>
         <label>아이디 <input type="text" name="id" required placeholder="아이디를 입력해주세요"></label>
         <label>비밀번호 <input type="password" name="pw" required placeholder="비밀번호를 입력해주세요"></label>
         <label>이름 <input type="text" name="name" required placeholder="이름를 입력해주세요"></label>
